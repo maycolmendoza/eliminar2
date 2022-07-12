352b813623c5a7bedddec411e713d24f5103b741
maycolmendoza <105061519+maycolmendoza@users.noreply.github.com>
2022-07-12 12:28:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -29,33 +29,7 @@
     <script src="views/app.js"></script>
 
 </head>
-<!-- Messenger Plugin de chat Code -->
-<div id="fb-root"></div>
-<!-- Your Plugin de chat code -->
-<div id="fb-customer-chat" class="fb-customerchat">
-</div>
-<script>
-    var chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("page_id", "111372388237568");
-    chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-<!-- Your SDK code -->
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml: true,
-            version: 'v14.0'
-        });
-    };
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
 <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
